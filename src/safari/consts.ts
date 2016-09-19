2ab668ef0b21725dfb978a8a9214daa8baa5966c
Alex Fomin <sane@MacBook-Pro-Alex.local>
2016-09-19 17:05:06 +0300

    Add lodash

---
+++ src/safari/consts.ts
@@ -0,0 +1,5 @@
+export class ExtensionButtons {
+    public static get ADD_TO_POCKET(): string {
+        return 'add_to_pocket';
+    }
+}
--- src/safari/index.ts
+++ src/safari/index.ts
@@ -1,2 +1,12 @@
 /// <reference path="./safari.d.ts"/>
-//safari.
+/// <reference path="../../typings/index.d.ts"/>
+import _ = require("lodash");
+import {ExtensionButtons} from "./consts";
+
+
+console.log(_);
+
+let itemArray = safari.extension.toolbarItems;
+let button = _.find(itemArray, x=>x.identifier == ExtensionButtons.ADD_TO_POCKET);
+
+console.log(button);
